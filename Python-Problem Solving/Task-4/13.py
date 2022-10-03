2973c128283f7e90a1d5d1d0e119af599ab66502
Mani Teja <102532741+ManiTeja13@users.noreply.github.com>
2022-10-03 23:51:25 +0530

    Add files via upload

---
+++ Python-Problem Solving/Task-4/13.py
@@ -0,0 +1,17 @@
+def minion_game(string):
+    # your code goes here
+    scores = {"Kevin": 0, "Stuart": 0}
+    for i in range(len(string)):
+        if string[i] in "AEIOU":
+            scores["Kevin"]+=len(string)-i
+        else:
+            scores["Stuart"]+=len(string)-i
+    if scores["Stuart"] == scores["Kevin"]:
+        print("Draw")
+    elif scores["Stuart"] > scores["Kevin"]:
+        print("%s %s" %("Stuart", scores["Stuart"]))
+    else:
+        print("%s %s" %("Kevin", scores["Kevin"]))
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
